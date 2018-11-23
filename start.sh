ae944db8be4311892b028cda49e5ce14dad14129
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-11-23 15:58:37 +0300

    modified start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-export SECRET_KEY='franco'
-export MAIL_USERNAME='sakwafrancis89@gmail.com'
-export MAIL_PASSWORD='francis2018'
+ SECRET_KEY='franco'
+ MAIL_USERNAME='sakwafrancis89@gmail.com'
+ MAIL_PASSWORD='francis2018'
 
-python3.6 manage.py server
